df9a91d227d3900d7f3431cf5847d631769ce05e
Diego Volpatto <volpatto@lncc.br>
2025-06-13 18:38:29 -0300

    Major refactoring in dpp.py adding dpp_delayed_form

--- src/perphil/forms/dpp.py
+++ src/perphil/forms/dpp.py
@@ -4,6 +4,94 @@ import firedrake as fd
 from perphil.models.dpp.parameters import DPPParameters
 
 
+def _calculate_mass_transfer_term(
+    p_macro: fd.Function,
+    p_micro: fd.Function,
+    beta: fd.Constant,
+    mu: fd.Constant,
+) -> fd.Function:
+    """
+    TODO.
+
+    :param p_macro: _description_
+    :type p_macro: fd.Function
+    :param p_micro: _description_
+    :type p_micro: fd.Function
+    :param beta: _description_
+    :type beta: fd.Constant
+    :param mu: _description_
+    :type mu: fd.Constant
+    :return: _description_
+    :rtype: fd.Function
+    """
+    xi = -beta / mu * (p_macro - p_micro)
+    return xi
+
+
+def _macro_scale_form(
+    trial_and_test_functions_macro: Tuple[fd.Function, fd.Function],
+    pressure_micro: fd.Function,
+    k1: fd.Constant,
+    mu: fd.Constant,
+    beta: fd.Constant,
+) -> Tuple[fd.Form, fd.Form]:
+    """
+    TODO.
+
+    :param trial_and_test_functions_macro: _description_
+    :type trial_and_test_functions_macro: Tuple[fd.Function, fd.Function]
+    :param pressure_micro: _description_
+    :type pressure_micro: fd.Function
+    :param k1: _description_
+    :type k1: fd.Constant
+    :param mu: _description_
+    :type mu: fd.Constant
+    :param beta: _description_
+    :type beta: fd.Constant
+    :return: _description_
+    :rtype: Tuple[fd.Form, fd.Form]
+    """
+    p1, q1 = trial_and_test_functions_macro
+    p2 = pressure_micro
+    xi = _calculate_mass_transfer_term(p1, p2, beta, mu)
+    a_macro = (k1 / mu) * fd.inner(fd.grad(p1), fd.grad(q1)) * fd.dx - xi * q1 * fd.dx
+    L_macro = fd.Constant(0.0) * q1 * fd.dx
+
+    return a_macro, L_macro
+
+
+def _micro_scale_form(
+    trial_and_test_functions_micro: Tuple[fd.Function, fd.Function],
+    pressure_macro: fd.Function,
+    k2: fd.Constant,
+    mu: fd.Constant,
+    beta: fd.Constant,
+) -> Tuple[fd.Form, fd.Form]:
+    """
+    TODO.
+
+    :param trial_and_test_functions_micro: _description_
+    :type trial_and_test_functions_micro: Tuple[fd.Function, fd.Function]
+    :param pressure_macro: _description_
+    :type pressure_macro: fd.Function
+    :param k2: _description_
+    :type k2: fd.Constant
+    :param mu: _description_
+    :type mu: fd.Constant
+    :param beta: _description_
+    :type beta: fd.Constant
+    :return: _description_
+    :rtype: Tuple[fd.Form, fd.Form]
+    """
+    p1 = pressure_macro
+    p2, q2 = trial_and_test_functions_micro
+    xi = _calculate_mass_transfer_term(p1, p2, beta, mu)
+    a_micro = (k2 / mu) * fd.inner(fd.grad(p2), fd.grad(q2)) * fd.dx + xi * q2 * fd.dx
+    L_micro = fd.Constant(0.0) * q2 * fd.dx
+
+    return a_micro, L_micro
+
+
 def dpp_form(W: fd.FunctionSpace, model_params: DPPParameters) -> Tuple[fd.Form, fd.Form]:
     """
     Build the bilinear and linear forms for the double-porosity/permeability system.
@@ -34,18 +122,71 @@ def dpp_form(W: fd.FunctionSpace, model_params: DPPParameters) -> Tuple[fd.Form,
     beta = model_params.beta
     mu = model_params.mu
 
-    xi = -beta / mu * (p1 - p2)
+    a_macro, L_macro = _macro_scale_form((p1, q1), p2, k1, mu, beta)
 
-    a_macro = (k1 / mu) * fd.inner(fd.grad(p1), fd.grad(q1)) * fd.dx - xi * q1 * fd.dx
-    L_macro = fd.Constant(0.0) * q1 * fd.dx
-
-    a_micro = (k2 / mu) * fd.inner(fd.grad(p2), fd.grad(q2)) * fd.dx + xi * q2 * fd.dx
-    L_micro = fd.Constant(0.0) * q2 * fd.dx
+    a_micro, L_micro = _micro_scale_form(p1, (p2, q2), k2, mu, beta)
 
     a = a_macro + a_micro
     L = L_macro + L_micro
 
     return a, L
+
+
+def dpp_delayed_form(W: fd.FunctionSpace, model_params: DPPParameters) -> Tuple[fd.Form, fd.Form]:
+    """
+    Build the bilinear and linear forms for the double-porosity/permeability system with delayed
+    pressure fields.
+
+    This function is to be used only with the Picard approach.
+
+    :param W:
+        A MixedFunctionSpace for (p1, p2) pressures.
+
+    :param model_params:
+        DPPParameters container with model constants.
+
+    :return:
+        Tuple of ((a_macro, L_macro), (a_micro, L_micro)), where:
+
+        - a_macro is the bilinear form for the macro system.
+        - a_micro is the bilinear form for the micro system.
+        - L_macro is the linear form associated with the macro scale.
+        - L_micro is the linear form associated with the micro scale.
+
+    :raises ValueError:
+        If W is not a MixedFunctionSpace.
+    """
+    if not isinstance(W, fd.MixedFunctionSpace):
+        raise ValueError(f"Expected a MixedFunctionSpace for W, got {type(W)}")
+
+    # Trial and test functions
+    p1 = fd.TrialFunction(W.sub(0))
+    p2 = fd.TrialFunction(W.sub(1))
+    q1 = fd.TestFunction(W.sub(0))
+    q2 = fd.TestFunction(W.sub(1))
+
+    # Model parameters
+    k1 = model_params.k1
+    k2 = model_params.k2
+    assert isinstance(k2, fd.Constant)
+    beta = model_params.beta
+    mu = model_params.mu
+
+    # Initial delayed pressures (assumed zero)
+    p1_old = fd.interpolate(fd.Constant(0), W.sub(0))
+    p2_old = fd.interpolate(fd.Constant(0), W.sub(1))
+
+    ## Macro terms
+    forms_macro = _macro_scale_form((p1, q1), p2_old, k1, mu, beta)
+    a_macro = fd.lhs(forms_macro[0])
+    L_macro = fd.rhs(forms_macro[0]) + forms_macro[1]
+
+    ## Micro terms
+    forms_micro = _micro_scale_form((p2, q2), p1_old, k2, mu, beta)
+    a_micro = fd.lhs(forms_micro[0])
+    L_micro = fd.rhs(forms_micro[0]) + forms_micro[1]
+
+    return (a_macro, L_macro), (a_micro, L_micro)
 
 
 def dpp_splitted_form(
